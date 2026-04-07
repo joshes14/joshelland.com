a429fbe9b2a084fd71f4e91015c73e5f689bd14f
frelsecoding <joshelland.sombrano@cvsu.edu.ph>
2026-04-07 08:40:27 +0800

    Removed images in gitignore, resulting of no pictures in the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://172.188.219.55:30080',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
